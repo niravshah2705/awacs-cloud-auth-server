4026467a24ff6d9b2bb710d999059fccaf34f21e
girishaiocdawacs <girish.mahajan@aiocdawacs.com>
2020-11-06 22:15:21 +0530

    stackdriver

--- src/main/java/com/aiocdwacs/awacscloudauthserver/config/WebSecurityConfigurer.java
+++ src/main/java/com/aiocdwacs/awacscloudauthserver/config/WebSecurityConfigurer.java
@@ -3,7 +3,6 @@ package com.aiocdwacs.awacscloudauthserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
