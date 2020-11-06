6a947ba68fa34c7687d83c67c7e84273bc4178b8
girishaiocdawacs <girish.mahajan@aiocdawacs.com>
2020-11-07 00:17:21 +0530

    mysql back

--- src/main/java/com/aiocdwacs/awacscloudauthserver/model/User.java
+++ src/main/java/com/aiocdwacs/awacscloudauthserver/model/User.java
@@ -16,7 +16,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User implements Serializable {
     public User() {
     }
