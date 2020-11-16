ccc701f9b5bf3762c5e0ef45b73316a4241a5c24
girishaiocdawacs <girish.mahajan@aiocdawacs.com>
2020-11-16 13:27:13 +0530

    snapshot 1.0.12 version change

--- src/main/java/com/aiocdwacs/awacscloudauthserver/AwacsCloudAuthServerApplication.java
+++ src/main/java/com/aiocdwacs/awacscloudauthserver/AwacsCloudAuthServerApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+import com.aiocdawacs.cloud.stream.config.EnableAwacsEventPublisherModule;
+
 @SpringBootApplication
 @EnableAuthorizationServer
+@EnableAwacsEventPublisherModule
 public class AwacsCloudAuthServerApplication {
 
 	public static void main(String[] args) {
