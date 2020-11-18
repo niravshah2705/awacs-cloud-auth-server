46b6e4447c5dca32f04bc62e7f539bb84f92e58b
girishaiocdawacs <girish.mahajan@aiocdawacs.com>
2020-11-18 10:54:17 +0530

    swagger fox 3.0.0

--- src/main/java/com/aiocdwacs/awacscloudauthserver/config/OAuth2Config.java
+++ src/main/java/com/aiocdwacs/awacscloudauthserver/config/OAuth2Config.java
@@ -1,13 +1,18 @@
 package com.aiocdwacs.awacscloudauthserver.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @Configuration
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
@@ -25,15 +30,15 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 		clients
 		.inMemory()
 		.withClient("a")//client username: a
-		.secret("a")//password: a
+		.secret(passwordEncoder().encode("a"))//password: a
 		.authorities("ROLE_A","ROLE_B","ROLE_TRUSTED_CLIENT")
 		.scopes("all")
 		.authorizedGrantTypes("client_credentials")
 		.and()
 		.withClient("b")
-		.secret("b")
+		.secret(passwordEncoder().encode("b"))
 		.authorities("ROLE_C")
-		.scopes("all")
+		.scopes("all", "read", "write")
 		.authorizedGrantTypes("refresh_token", "password", "client_credentials")
 		.accessTokenValiditySeconds(1200)
 		.refreshTokenValiditySeconds(240000);
@@ -46,15 +51,15 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 		security.checkTokenAccess("hasAuthority('ROLE_C')"); //secure the oauth/check_token endpoint
 	}
 
-//	@Bean
-//	public TokenStore tokenStore() {
-//		return new InMemoryTokenStore();
-//	}
+	@Bean
+	public TokenStore tokenStore() {
+		return new InMemoryTokenStore();
+	}
 
-//	@Bean
-//	public PasswordEncoder passwordEncoder(){
-//		return new BCryptPasswordEncoder(4);
-//	}
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder(4);
+	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
