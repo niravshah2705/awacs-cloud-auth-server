6ac6ddc28f14a54db43190753aa11be50a95d905
girishaiocdawacs <girish.mahajan@aiocdawacs.com>
2020-11-24 02:29:44 +0530

    Fake

--- src/main/java/com/aiocdwacs/awacscloudauthserver/controller/UserController.java
+++ src/main/java/com/aiocdwacs/awacscloudauthserver/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.aiocdwacs.awacscloudauthserver.controller;
 
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -34,6 +36,12 @@ class UserController {
    UserController(UserRepository repository, PasswordEncoder passwordEncoder) {
        this.repository = repository;
        this.passwordEncoder = passwordEncoder;
+   }
+   
+   @GetMapping("/me")
+   @PreAuthorize("hasRole('SYSTEM')")
+   public ResponseEntity<Principal> get(final Principal principal) {
+       return ResponseEntity.ok(principal);
    }
 
    @GetMapping
