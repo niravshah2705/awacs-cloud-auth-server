9a200aae1d37c93b1c99abb1b17ea579cd19ac28
girishaiocdawacs <girish.mahajan@aiocdawacs.com>
2020-11-25 07:17:46 +0530

    jmeter plan

--- src/main/resources/data-mysql/01.sql
+++ src/main/resources/data-mysql/01.sql
@@ -32,5 +32,5 @@ insert into oauth_client_details(client_id, resource_ids, client_secret, scope, 
 insert into oauth_client_details(client_id, resource_ids, client_secret, scope, authorized_grant_types, authorities, access_token_validity, refresh_token_validity)
 	values ('bluesky', 'resource-server-rest-api',
 	/*bluesky*/'$2y$08$1k6aEldpcDovzErVENCjL.BZzqMP/0zoF9T/z859FgxPoQL90Nq2O',
-	'read,write', 'password,authorization_code,refresh_token', 'USER', 10800, 2592000);
+	'read,write', 'client_credentials,password,authorization_code,refresh_token', 'JMETER', 10800, 2592000);
 	
