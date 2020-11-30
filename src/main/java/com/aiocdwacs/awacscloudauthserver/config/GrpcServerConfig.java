6c4d242f0bfce4cc60c3461d9b0b1f49d33f39ba
girishaiocdawacs <girish.mahajan@aiocdawacs.com>
2020-11-30 19:46:20 +0530

    gRPC implementation for check token endpoin

--- src/main/java/com/aiocdwacs/awacscloudauthserver/config/GrpcServerConfig.java
+++ src/main/java/com/aiocdwacs/awacscloudauthserver/config/GrpcServerConfig.java
@@ -6,11 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
+import net.devh.boot.grpc.server.security.authentication.BasicGrpcAuthenticationReader;
+import net.devh.boot.grpc.server.security.authentication.GrpcAuthenticationReader;
 import net.devh.boot.grpc.server.serverfactory.GrpcServerConfigurer;
 
 @Configuration
 public class GrpcServerConfig {
 
+	@Bean 
+	public GrpcAuthenticationReader grpcAuthenticationReader() {
+		return new BasicGrpcAuthenticationReader();
+	}
+	
 	@Bean
 	public GrpcServerConfigurer keepAliveServerConfigurer() {
 	    return serverBuilder -> {
